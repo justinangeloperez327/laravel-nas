3562e84de5c30f32285258ed46228edbe1a0d792
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-18 16:13:34 +0400

    Add Inertia root view

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        @viteReactRefresh
+        @vite(['resources/css/app.css', 'resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
+
+        <x-inertia::head>
+            <title>{{ config('app.name', 'Noor Al Sahara') }}</title>
+        </x-inertia::head>
+    </head>
+    <body class="antialiased">
+        <x-inertia::app />
+    </body>
+</html>
