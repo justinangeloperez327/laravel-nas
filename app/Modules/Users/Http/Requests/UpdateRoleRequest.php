105b86a047c8245b596fffcd10b25d3f195663d0
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-18 16:57:18 +0400

    Authorize UpdateRoleRequest.php through policy

--- app/Modules/Users/Http/Requests/UpdateRoleRequest.php
+++ app/Modules/Users/Http/Requests/UpdateRoleRequest.php
@@ -10,7 +10,7 @@ class UpdateRoleRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return true;
+        return $this->user()?->can('update', $this->route('role')) ?? false;
     }
 
     /**
