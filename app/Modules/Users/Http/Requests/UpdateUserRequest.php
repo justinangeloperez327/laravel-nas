a9d7863ac100619148087acfce23c12af9a566b9
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-18 16:47:06 +0400

    Add update user request

---
+++ app/Modules/Users/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Modules\Users\Http\Requests;
+
+use App\Modules\Users\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
+
+class UpdateUserRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * @return array<string,mixed>
+     */
+    public function rules(): array
+    {
+        /** @var User $user */
+        $user = $this->route('user');
+
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => [
+                'required',
+                'email',
+                'max:255',
+                Rule::unique('users', 'email')->ignore($user->id),
+            ],
+            'password' => ['nullable', 'string', Password::default(), 'confirmed'],
+            'role_ids' => ['required', 'array'],
+            'role_ids.*' => ['integer', 'exists:roles,id'],
+        ];
+    }
+}
