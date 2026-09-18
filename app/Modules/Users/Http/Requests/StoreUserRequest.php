6e4e3f6dd71efcf80375ae9b6802f9258b411677
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-18 16:57:04 +0400

    Authorize StoreUserRequest.php through policy

--- app/Modules/Users/Http/Requests/StoreUserRequest.php
+++ app/Modules/Users/Http/Requests/StoreUserRequest.php
@@ -9,7 +9,7 @@ class StoreUserRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return true;
+        return $this->user()?->can('create', \App\Modules\Users\Models\User::class) ?? false;
     }
 
     /**
