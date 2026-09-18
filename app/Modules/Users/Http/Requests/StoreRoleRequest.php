6fa76164c0dc0b37d97c260ce314fcaecaecd871
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-18 16:57:15 +0400

    Authorize StoreRoleRequest.php through policy

--- app/Modules/Users/Http/Requests/StoreRoleRequest.php
+++ app/Modules/Users/Http/Requests/StoreRoleRequest.php
@@ -8,7 +8,7 @@ class StoreRoleRequest extends FormRequest
 {
     public function authorize(): bool
     {
-        return true;
+        return $this->user()?->can('create', \App\Modules\Users\Models\Role::class) ?? false;
     }
 
     /**
