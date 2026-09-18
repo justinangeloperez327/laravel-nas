3f79679a501d5c54f16c9feb2830598972c16fdd
Clover <68591353+justinangeloperez327@users.noreply.github.com>
2026-09-18 16:45:44 +0400

    Add role model

---
+++ app/Modules/Users/Models/Role.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Modules\Users\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Role extends Model
+{
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+    ];
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class)
+            ->withTimestamps();
+    }
+
+    public function permissions(): BelongsToMany
+    {
+        return $this->belongsToMany(Permission::class)
+            ->withTimestamps();
+    }
+}
